1d578136a7f6d19a50f2bed140eb0cc29358e9d5
NguyenLe2004 <nguyenlequangdt@gmail.com>
2024-05-04 12:52:06 +0700

    add login form

--- src/Home/SignInForm/SignInForm.js
+++ src/Home/SignInForm/SignInForm.js
@@ -1,17 +1,27 @@
-import React, { useState} from 'react'
+import React, { useState,useEffect} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/esm/Button';
 import hospitalImg from "../../Image/hospital_cartoon.jpeg"
+import logo from "../../Image/logo.png"
 import "./SignInForm.css"
 
 const SignInForm = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [validated, setValidated] = useState(false);
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [isDisplayEyeIcon, setIsDisplayEyeIcon] = useState(false);
+  const [isCapsLockOn, setIsCapsLockOn] = useState(false);
 
+  const handleKeyPress = (event) => {
+    if (event.getModifierState && event.getModifierState('CapsLock')) {
+      setIsCapsLockOn(true);
+    } else {
+      setIsCapsLockOn(false);
+    }
+  };
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
@@ -33,26 +43,35 @@ const SignInForm = () => {
     setIsLoading(false);
   }
   
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
 
   return (
   <div className='outer-sign-in' >
     <div style={{display:"flex", position:"relative",width:"fit-content",borderRadius:"10px",overflow:"hidden",height:"80vh"}}>
         <img className="backgound" src={hospitalImg} />
         <Form noValidate className="loginForm" validated={validated} onSubmit={handleSubmit}>
-            <h1>Đăng nhập</h1> 
+        <div style={{display: "flex",justifyContent:"center",alignItems:"center",fontWeight:"bold",flexDirection:"column",marginBottom:"2vh"}}>
+          <img style={{height:"15vh",width:"15vw"}} src={logo} />
+          <div style={{fontWeight:"bold",fontSize:"60px",marginTop:"2vh"}}>Đăng nhập</div>
+        </div>
             <Form.Group className="mb-3"  controlId="Email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control pattern=".+@.+\.[A-Za-z]+$" required type="email" placeholder="Enter email"/>
+                <Form.Label style={{fontWeight:"bold"}}>Email</Form.Label>
+                <Form.Control pattern=".+@.+\.[A-Za-z]+$" required type="email" placeholder="name@example.com"/>
                 <Form.Control.Feedback type='invalid'  >Email không hợp lệ</Form.Control.Feedback>
             </Form.Group> 
             <Form.Group style={{position:"relative"}} className="mb-3" controlId="Password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control required type={passwordVisible ? 'text' : 'password'} placeholder="Password"/>
-                <FontAwesomeIcon className='eye-icon' onClick={togglePasswordVisibility} icon={passwordVisible ? faEyeSlash : faEye} />
+                <Form.Label style={{fontWeight:"bold"}}>Password</Form.Label>
+                <Form.Control onClick={()=>setIsDisplayEyeIcon(true)} required type={passwordVisible ? 'text' : 'password'} placeholder="Password"/>
+                <FontAwesomeIcon className='eye-icon' onClick={togglePasswordVisibility} icon={passwordVisible ?  faEye : faEyeSlash} />
                 <Form.Control.Feedback type="invalid">Password không hợp lệ</Form.Control.Feedback>
             </Form.Group>
             <div className="submitBtn">
-              <Button disabled={isLoading} variant="primary" type="submit">
+              <Button disabled={isLoading} style={{width:"100%",marginTop:"2vh"}} variant="primary" type="submit">
                 {isLoading? "Loading...." : "submit"}
               </Button>
             </div>
