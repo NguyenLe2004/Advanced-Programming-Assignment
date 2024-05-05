8274c9db278f9c44eff5acaa4b70a2b89d6633e0
thyenbvck <bach.hoangthienbach@hcmut.edu.vn>
2024-05-06 01:59:18 +0700

    filterAndSearch of all object

--- src/Patient/DisplayDataPatient/FilterAndSearch/FilterAndSearch.js
+++ src/Patient/DisplayDataPatient/FilterAndSearch/FilterAndSearch.js
@@ -3,33 +3,85 @@ import { Form, Row,Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 import "./FilterAndSearch.css"
-
-const FilterAndSearch = ({setDataMedicineDisplay, dataMedicineDisplay}) => {
+import moment from 'moment';
+const FilterAndSearch = ({setDataPatientDisplay, dataPatient}) => {
     const [isClickSearchIcon , setIsClickSearchIcon ] =useState(false)
+    const [searchValue, setSearchValue] = useState("");
     const searchBlockRef = useRef(null)
     const handleClickOutside = (event) => {
         if (searchBlockRef.current && !searchBlockRef.current.contains(event.target)) {
             setIsClickSearchIcon(false);
         }
       };
+      const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            searchByCitizenID(searchValue);
+        }
+    }
+    const searchByCitizenID =(citizenID) =>{
+        if(citizenID===""){
+            setDataPatientDisplay(dataPatient)
+        }
+        const searchedData = dataPatient.filter(item => item.citizenID === citizenID);
+        setDataPatientDisplay(searchedData);
+    }
+    const filterDataByStatus = (status) => {
+        if (status === "") {
+            setDataPatientDisplay(dataPatient);
+        } else {
+            const filteredData = dataPatient.filter(item => item.status === status);
+            setDataPatientDisplay(filteredData);
+        }
+    }
+    const filterDataByGender = (gender) => {
+        if (gender === "") {
+            setDataPatientDisplay(dataPatient);
+        } else {
+            const filteredData = dataPatient.filter(item => item.gender === gender);
+            setDataPatientDisplay(filteredData);
+        }
+    }
+    const filterDataByAge = (age) => {
+        if (age === "") {
+            setDataPatientDisplay(dataPatient);
+        } else {
+            const Date = moment();
+            const filteredData = dataPatient.filter(item => {
+                const birthDayOfPatient = moment(item.dateOfBirth)
+                const ageOfPatient = Date.diff(birthDayOfPatient, 'years');
+                if (age === "< 18 tuổi") {
+                    return ageOfPatient < 18;
+                } else if (age === ">= 18 tuổi") {
+                    return ageOfPatient >= 18;
+                }
+            });
+            setDataPatientDisplay(filteredData);
+        }
+    }
     useEffect(() => {
-    document.addEventListener('click', handleClickOutside);
-    return () => {
-        document.removeEventListener('click', handleClickOutside);
-    };
-    }, []);
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+        }, []);
   return (
     <div className='search-filter-container' >
         <span className='search-block' ref={searchBlockRef}>
             <Form className={`search-form ${isClickSearchIcon ? 'active' : ""}`}>
                 <Form.Group >
-                    <Form.Control type='search' placeholder='Nhập tên thuốc' pattern="^[a-zA-Z0-9]+$" />
+                    <Form.Control type='search' placeholder='Nhập CCCD'  pattern="^\d{12}"
+                             onChange={(e) => setSearchValue(e.target.value)}
+                             onKeyDown={handleKeyDown}
+                           />
             <Form.Control.Feedback type="invalid">
-             Tên không hợp lệ
+             CCCD không hợp lệ
             </Form.Control.Feedback>
                 </Form.Group>
             </Form>
-            <span className={`search-icon ${isClickSearchIcon ? 'active' : ""}`} onClick={() => setIsClickSearchIcon(true)} >
+            <span className={`search-icon ${isClickSearchIcon ? 'active' : ""}`} onClick={() => {
+                setIsClickSearchIcon(true)
+                }} >
                 <FontAwesomeIcon icon={faMagnifyingGlass} />
             </span>
         </span>
@@ -39,12 +91,36 @@ const FilterAndSearch = ({setDataMedicineDisplay, dataMedicineDisplay}) => {
                     <Row>
                     <Col>
                         <Form.Group>
-                            <Form.Select>
-                                <option>Trạng thái</option>
-                                <option>Sẵn sàng</option>
-                                <option>Hết hạn</option>
-                                <option>Hết thuốc</option>
-                            </Form.Select>
+                        <Form.Select onChange={(e) => {
+                                        filterDataByStatus(e.target.value);
+                                    }}>
+                                        <option value="">Trạng thái</option>
+                                        <option value="Hoàn thành điều trị">Hoàn thành điều trị</option>
+                                        <option value="Đang điều trị">Đang điều trị</option>
+                                        <option value="Chưa điều trị">Chưa điều trị</option>
+                                    </Form.Select>
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group>
+                        <Form.Select onChange={(e) => {
+                                        filterDataByGender(e.target.value);
+                                    }}>
+                                        <option value="">Giới tính</option>
+                                        <option value="Nam">Nam</option>
+                                        <option value="Nữ">Nữ</option>
+                                    </Form.Select>
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group>
+                        <Form.Select onChange={(e) => {
+                                        filterDataByAge(e.target.value);
+                                    }}>
+                                        <option value="">Tuổi</option>
+                                        <option value="< 18 tuổi">Dưới 18 tuổi</option>
+                                        <option value=">= 18 tuổi">Trên 18 tuổi</option>
+                                    </Form.Select>
                         </Form.Group>
                     </Col>
                     <Col>
